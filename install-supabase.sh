69b2a0d3b0fb83c242f2d156acc6804ef6af6913
Igor-Nersisyan <igor.nersisyan@gmail.com>
2025-11-30 22:32:40 -0700

    Update install-supabase.sh

--- install-supabase.sh
+++ install-supabase.sh
@@ -1646,7 +1646,7 @@ case $OPTION in
         echo -e "  Host:     ${GREEN}host.docker.internal${NC}"
         echo -e "  Port:     ${GREEN}5432${NC}"
         echo -e "  Database: ${GREEN}postgres${NC}"
-        echo -e "  User:     ${GREEN}postgres${NC}"
+        echo -e "  User:     ${GREEN}postgres.postgres${NC}"
         echo -e "  Password: ${GREEN}<from /root/supabase-credentials.txt>${NC}"
         echo -e "  SSL:      ${GREEN}Disable${NC}"
         echo ""
